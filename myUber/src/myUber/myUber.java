f72b6d39ea34255f4711d232a57226c7505615a3
captainrainb0w <damiani.benoit.jp@gmail.com>
2018-11-08 17:12:26 +0100

    Ajout des types de ride

--- myUber/src/myUber/myUber.java
+++ myUber/src/myUber/myUber.java
@@ -6,6 +6,16 @@ public class myUber {
 	
 	protected ArrayList<String> bookOfRides;
 	
+	public String getTraffic(int heure) {
+		
+		return "low";
+	}
+	
+	public double rideCost(String rideType, double length, String traffic) {
+		
+		return 1.2;
+	}
+	
 	public static void main(String[] args) {
 		
 	}
