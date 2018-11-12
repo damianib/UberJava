88d6b392cad3e4c7ef3f8eba211ae752d400b5c2
captainrainb0w <damiani.benoit.jp@gmail.com>
2018-11-12 16:53:46 +0100

    Ajout d'une fonction dans myUber pour déterminer aléatoirement le traffic

--- myUber/src/myUber/myUber.java
+++ myUber/src/myUber/myUber.java
@@ -2,6 +2,47 @@ package myUber;
 
 public class MyUber {
 	
+	public static String getTraffic(int heure) {
+		
+		double plow;
+		double pmedium;
+		double phigh;
+		
+		if (heure>22 || heure<7) {
+			plow = 0.95;
+			pmedium = 0.04;
+			phigh = 0.01;
+		}
+		else if (heure < 11) {
+			plow = 0.05;
+			pmedium = 0.2;
+			phigh = 0.75;
+		}
+		else if (heure < 17) {
+			plow = 0.15;
+			pmedium = 0.7;
+			phigh = 0.15;
+		}
+		else {
+			plow = 0.01;
+			pmedium = 0.04;
+			phigh = 0.95;
+		}
+		
+		double tirage = Math.random();
+		
+		if (tirage < plow) {
+			return "low";
+		}
+		else if (tirage < plow + pmedium) {
+			return "medium";
+		}
+		else {
+			return "high";
+		}
+		
+	}
+	
 	public static double rate(Ride ride, double length, String traffic) {
 		
 		return ride.basicRate(length)*length*ride.trafficRate(traffic);
