83c42806d500df6e53014d734a42fc21bcd87587
Matthieu Pillot <matthieudecoligny@yahoo.fr>
2018-11-13 21:09:57 +0100

    continution dans la main
    ajout d'une classe spéciale test (owi !)

--- myUber/src/myUber/MyUber.java
+++ myUber/src/myUber/MyUber.java
@@ -75,6 +75,22 @@ public class MyUber {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Customer Client = recupererOuCreerClient(sc);
+		ArrayList<Double> Depart = GPSDepart(sc);
+		ArrayList<Double> Destination = GPSArrivee(sc);
+		double distance = getDistance (Depart, Destination);
+		String traffic = getTraffic();
+		String TypeDeTrajet = demanderTrajet(distance, traffic, sc);
+		int nbrPlaceVoulues = demanderNbrPlaces(TypeDeTrajet, sc);
+		
+		
+		
+		
+		
+	}	
+	
+	public static Customer recupererOuCreerClient(Scanner sc) {
+		
 		System.out.println("quel est votre identifiant client ?");
 		String idDuClientSTR =sc.nextLine();
 		int idDuClient = Integer.parseInt(idDuClientSTR);
@@ -96,9 +112,88 @@ public class MyUber {
 			NewGps.add(0.);
 			
 			
-			Customer s = new Customer (NewName, NewSurname, NewID, NewGps, NewCard);
+			return ( new Customer (NewName, NewSurname, NewID, NewGps, NewCard) );
 			
+		} else {
+			/// ici il faut faire une fonction qui récupère l'objet client existant à partir de son id, je sais pas faire
+			ArrayList<Double> NewGps = new ArrayList<Double>();
+			NewGps.add(0.);
+			NewGps.add(0.);
+			return ( new Customer ("Pillot", "Wolpertinger", 424242, NewGps, "5875698451") );
 		}
-		
+	}
 
+	public static ArrayList<Double> GPSDepart(Scanner sc) {
+		System.out.println("où êtes vous ? Rentrez vos coordonnées GPS");
+		System.out.println("en commençant par x");
+		String X = sc.nextLine();
+		double newX = Double.parseDouble(X);
+		System.out.println("puis maintenant y");
+		String Y = sc.nextLine();
+		double newY = Double.parseDouble(Y);
+		ArrayList<Double> Depart = new ArrayList<Double>();
+		Depart.add(newX);
+		Depart.add(newY);
+		return Depart;
+	}
+	
+	public static ArrayList<Double> GPSArrivee(Scanner sc) {
+		System.out.println("où voulez vous aller ? Rentrez les coordonnées GPS");
+		System.out.println("en commençant par x");
+		String DX = sc.nextLine();
+		double DestX = Double.parseDouble(DX);
+		System.out.println("puis maintenant y");
+		String DY = sc.nextLine();
+		double DestY = Double.parseDouble(DY);
+		ArrayList<Double> Destination = new ArrayList<Double>();
+		Destination.add(DestX);
+		Destination.add(DestY);
+		return Destination;
+	}
+
+	public static String demanderTrajet(double distance, String traffic, Scanner sc) {
+		
+		/// il faut récuperer les quatre prix disponible en fonction du type de trajet, pour cela il faut utiliser la fonction rate que j'ai pas comprise ^^
+		double prixBlack = 42.;
+		double prixPool = 42.;
+		double prixVan = 42.;
+		double prixX = 42.;
+		/// les prix doivent être changés
+				
+		System.out.println("Pour votre trajet, en fonction des conditions actuelles de circulation, les tarifs sont les suivants");
+		System.out.println("UberBlack : " + prixBlack + " euros");
+		System.out.println("UberPool : " + prixPool + " euros");
+		System.out.println("UberVan : " + prixVan + " euros");
+		System.out.println("UberX : " + prixX + " euros");
+		System.out.println("Inscrivez le nom du transport choisi, ou tappez 'info' pour avoir les détails sur les différents rides disponibles");
+		String TypeDemande = sc.nextLine();
+		if (TypeDemande == "info") {
+			System.out.println("inserer ici un pavé de description des ride");
+			return demanderTrajet(distance, traffic, sc);
+		} else {
+			return TypeDemande;
+		}
+				
+	}
+	
+	public static Integer demanderNbrPlaces(String trajet, Scanner sc) {
+		int max = 4;
+		if (trajet == "UberVan") {
+			max = 6;
+		}
+		int nbr =42;
+		while (!(nbr <= max && nbr > 0)) {
+			System.out.println("pour ce trajet, le nombre de place maximal est " + max + ", combien en voulez vous ?");
+			String nbrSTR = sc.nextLine();
+			nbr = Integer.parseInt(nbrSTR);
+		}
+		return (nbr);
+	}
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ myUber/src/myUber/ZZZTest.java
@@ -0,0 +1,14 @@
+package myUber;
+
+public class ZZZTest {
+		
+	public static void main(String[] args) {
+		double nombre = Double.parseDouble("42.23");
+		System.out.println(nombre);
+		nombre ++;
+		System.out.println(nombre);
+		
+		
+		
+	}
+}
