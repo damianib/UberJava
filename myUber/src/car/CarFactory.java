b5bc3122146722e492c115e544ac246d064f8cd5
Matthieu Pillot <matthieudecoligny@yahoo.fr>
2018-11-15 20:38:14 +0100

    modif carfactory

--- myUber/src/car/CarFactory.java
+++ myUber/src/car/CarFactory.java
@@ -2,22 +2,43 @@ package car;
 
 public class CarFactory {
 	
-	public Car createCar(String CarType, int nbrVoitureDeCeType) {
-		
-		String str = Integer.toString(nbrVoitureDeCeType +1);
-		String ID = CarType + str;
+	int compteurStandard = 0;
+	int compteurVan = 0;
+	int compteurBerline = 0;
+	
+	public Car createCar(String CarType) {
 		
 		if (CarType == "Van") {
+			compteurVan ++;
+			String str = Integer.toString(compteurVan);
+			String ID = CarType + str;
 			return new Van(ID);
 		}
 		else if (CarType == "Standard") {
+			compteurStandard ++;
+			String str = Integer.toString(compteurStandard);
+			String ID = CarType + str;
 			return new Standard(ID);
 		}
 		else if (CarType == "Berline") {
+			compteurBerline ++;
+			String str = Integer.toString(compteurBerline);
+			String ID = CarType + str;
 			return new Berline(ID);
 		}
 		return null;
 	}
+	
+	public int getNbrOfStandard () {
+		return compteurStandard;
+	}
+	public int getNbrOfVan () {
+		return compteurVan;
+	}
+	public int getNbrOfBerline () {
+		return compteurBerline;
+	}
+	
 	
 	
 
